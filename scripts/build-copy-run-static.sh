dd14156df884ef28ffa84a6048986faae1e865b6
Joakim Frostegård <joakim.frostegard@gmail.com>
2017-01-02 15:36:20 +0100

    Add script for building app statically

---
+++ scripts/build-copy-run-static.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# This is problematic on macOS but might work on Linux
+# See https://ro-che.info/articles/2015-10-26-static-linking-ghc
+stack build --ghc-options='-optl-static -optl-pthread' --force-dirty &&
+    ./scripts/copy-cgi.sh &&
+    ./scripts/run.sh
